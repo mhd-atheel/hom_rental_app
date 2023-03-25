b757420b5dbfafce5c2c5c15de63396d1109c24a
mhd-atheel <aathilmazz1234@gmail.com>
2023-03-25 22:12:28 +0530

    bit changes

--- app/src/main/java/com/example/homerentalapp/MainActivity.java
+++ app/src/main/java/com/example/homerentalapp/MainActivity.java
@@ -11,9 +11,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getSupportActionBar().hide();   // this code for hiding the action bar
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        getSupportActionBar().hide();
+        // this code for hiding the action bar
         setContentView(R.layout.activity_main);
     }
 }
