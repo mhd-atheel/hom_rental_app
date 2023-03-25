12ed5bf3f43404ae193084ccb409f998db507e88
mhd-atheel <aathilmazz1234@gmail.com>
2023-03-25 21:24:55 +0530

    Initial commit

---
+++ app/src/main/java/com/example/homerentalapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.homerentalapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();   // this code for hiding the action bar
+        setContentView(R.layout.activity_main);
+    }
+}
